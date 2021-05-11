6783d980854291a7de1b36474f85b4f2f03dcf1c
Nataliia Breus <breusnataly@gmail.com>
2021-05-11 11:39:36 +0300

    timer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,10 @@
 'use strict';
+const timeContainer = document.querySelector(".timeContainer");
+const timerControlsContainer = document.querySelector(".timerControlsContainer");
+
+
+
+const timer = new Timer (timeContainer, timerControlsContainer);
 
 
 
